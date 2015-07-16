1bb96e158d2fb2b656a85be3ba9f50519d7d6a42
Pim <pim.amponpun@gmail.com>
2015-07-16 05:41:30 +0000

    2nd puppet

--- site.pp
+++ site.pp
@@ -1,6 +1,5 @@
-node 'wiki' {
-   class { 'base': }
-   
+node 'ip-10-0-1-55' {
+  class { 'base': }
 }
 class base {
 
@@ -28,37 +27,12 @@ class base {
    $puppetserverservice = $osfamily ? {
     'redhat' => 'puppet3-server',
     'debian' => 'puppet-server',
-     default  => 'puppet-server',
+    'Linux' => 'puppet3-server',
+     #default  => 'puppet-server',
   }
   
   package { $puppetserverservice:
     ensure  => 'installed',
   }
   
-  $puppet_dirs = [ "/etc/puppet/environments/production",
-  "/etc/puppet/environments/production/modules", 
-  "/etc/puppet/environments/production/manifests",]
-
-  file { $puppet_dirs:
-    ensure => "directory",
- }
- 
-  file { 'tempdir':
-    ensure => 'directory',
-    name   => '/home/vagrant/puppet'
-  }
-  
-  file { "/etc/puppet/environments/production/environment.conf":
-    ensure => "present",
-    source => '/home/vagrant/puppet/modules/base/environment.conf',
-       
- }
- 
- 
- 
- 
- 
- 
 }
-
-
